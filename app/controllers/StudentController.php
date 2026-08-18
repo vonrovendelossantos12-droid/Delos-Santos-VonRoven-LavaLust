7181cc421c76c0e3d09d54a3f35f6c9250d713f6
gamingdarkfox51-sudo <gamingdarkfox51@gmail.com>
2026-08-18 18:10:07 +0800

    Update profile page with user details - Von Roven A. Delos Santos (MCC2024-00051)

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -25,14 +25,14 @@ class StudentController extends Controller {
 		
 		// Create associative array with student data
 		$student = [
-			'student_id' => '2026-0001',
-			'name'       => 'Juan Dela Cruz',
+			'student_id' => 'MCC2024-00051',
+			'name'       => 'Von Roven A. Delos Santos',
 			'course'     => 'BS Information Technology',
-			'year'       => '2nd Year',
-			'section'    => 'A',
-			'email'      => 'juan@example.com',
-			'phone'      => '+63 917 123 4567',
-			'address'    => 'Manila, Philippines'
+			'year'       => '3rd Year',
+			'section'    => '3F2',
+			'email'      => 'vonrovendelossantos12@gmail.com',
+			'phone'      => '+09634013258',
+			'address'    => 'Gintong Diwa St. Camilmil Calapan, Oriental Mindoro, Philippines'
 		];
 		
 		// Pass data to view
